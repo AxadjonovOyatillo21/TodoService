fe36b5830abe8eb69cfceb86a95d75bc8b3e83e1
AxadjonovOyatillo21 <axadjonovoyatillo21@gmail.com>
2022-06-02 22:15:00 +0500

    Last commit

--- setup.sh
+++
@@ -1,8 +0,0 @@
-
-export FLASK_APP=app.py;
-export FLASK_ENV=development;
-export USERNAME=mathpy21;
-export PASSWORD=pysql21;
-export SERVER=localhost;
-export PORT=5432;
-export DB_NAME=todo_service;
